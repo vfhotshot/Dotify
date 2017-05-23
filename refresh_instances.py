98e767b2be0ee80bc432dff20e4da49efb494aaa
vfhotshot <vfhotshot@hotmail.com>
2017-05-23 10:25:12 -0400

    Making changes persist with AWS Console

--- refresh_instances.py
+++ refresh_instances.py
@@ -1,5 +1,7 @@
 import time
 import boto3
+import json
+import os
 
 LAMBDA_CLIENT = boto3.client('lambda')
 ASG_CLIENT = boto3.client('autoscaling')
@@ -20,19 +22,25 @@ def scale_up(asg_name):
     print "Updating " + asg_name + " desired capacity from " + str(desired_size) + " to " + str(double_desired)
     update_response = ASG_CLIENT.update_auto_scaling_group(AutoScalingGroupName=asg_name,
                                                            DesiredCapacity=double_desired)
+    
+    # Send Slack Notification
+    #os.system("curl -X POST --data-urlencode 'payload={\"channel\": \"#dotify\", \"username\": \"Tim-Bot\", \"text\": \"Refreshing DotifyA stack...\", \"icon_emoji\": \":thumbsup:\"}' https://hooks.slack.com/services/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")
+    
     # Wait after updating DesiredCapacity
-    sleep_time = 120
+    sleep_time = 90
     print "Sleeping for " + str(sleep_time) + " seconds for new instances to spin up..."
     time.sleep(sleep_time)  # delays for 120 seconds
 
     # Recursive:
-    event_payload = {'asg': asg_name, 'call_action': 'scale_down', 'size': desired_size}
-    invokeResponse = LAMBDA_CLIENT.invoke(
-        FunctionName='refresh_instances',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        Payload=json.dumps(event_payload)
-    )
+    #event_payload = {'asg': asg_name, 'call_action': 'scale_down', 'size': desired_size}
+    #invokeResponse = LAMBDA_CLIENT.invoke(
+     #   FunctionName='refresh_instances',
+     #   InvocationType='RequestResponse',
+     #   LogType='Tail',
+     #   Payload=json.dumps(event_payload)
+    #)
+    
+    scale_down(asg_name, desired_size)
     return None
 
 
@@ -49,11 +57,14 @@ def scale_down(asg_name, size_down):
 def lambda_handler(event, context):
     asg_name = event['asg']
     action = event['call_action']
+    scale_up(asg_name)
 
-    if action == 'scale_up':
-        scale_up(asg_name)
-    elif action == 'scale_down':
-        size_down = event['size']
-        scale_down(asg_name, size_down)
+    #if action == 'scale_down':
+    #    size_down = event['size']
+    #    scale_down(asg_name, size_down)
+    #elif action == 'scale_up':
+    #    scale_up(asg_name)
+    
+    os.system("curl -X POST --data-urlencode 'payload={\"channel\": \"#dotify\", \"username\": \"Tim-Bot\", \"text\": \"Completed Refresh of DotifyA stack...\", \"icon_emoji\": \":thumbsup:\"}' https://hooks.slack.com/services/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")
 
     return {'Completed': action}
